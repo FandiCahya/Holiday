83f4713db3bb510418a9d8e25db83ffe1ef2acb6
tempebenguk <erricksb592@gmail.com>
2024-01-08 20:43:53 +0700

    hwhwh

--- proyek/resources/views/auth/login.blade.php
+++ proyek/resources/views/auth/login.blade.php
@@ -1,35 +1,29 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <link
-      href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Montserrat&display=swap"
-      rel="stylesheet"
-    />
-    <link href="" rel="stylesheet" />
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="v8_50">
-      <div class="v8_57"></div>
-      <div class="v8_58"></div>
-      <div class="v8_59"></div>
-      <div class="v8_60"></div>
-      <div class="v8_61"></div>
-      <div class="v8_62"></div>
-      <span class="v8_63">Registrasi</span
-      ><span class="v8_64">Ingatkan saya</span
-      ><span class="v8_72">Belum punya akun?</span>
-      <div class="v8_65"></div>
-      <div class="v8_66"></div>
-      <span class="v8_67">LOGIN</span><span class="v8_69">USERNAME</span
-      ><span class="v8_70">PASSWORD</span>
-      <div class="v8_71"></div>
-      <div class="v8_73"></div>
-      <div class="v8_74"></div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="form-box">
+        <div class="wrapper">
+            <div class="img-area">
+                <img src="link gambar" alt="">
+            </div>
+            <div class="form-area">
+                <h3>Login</h3>
+            <form>
+                <div class="single-form">
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div class="single-form">
+                    <input type="password" placeholder="Password" />
+                </div>
+                <input type="submit" value="Login"/>
+            </form>
+            </div>
+        </div>
     </div>
-  </body>
+</body>
 </html>
